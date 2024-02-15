dfc44806c001b225575cec4184c673d30e5c437d
Hknots <hknots99@gmail.com>
2024-02-15 20:32:14 +0100

    add interface method

--- src/main/java/no/fint/model/FintMainObject.java
+++ src/main/java/no/fint/model/FintMainObject.java
@@ -1,4 +1,11 @@
 package no.fint.model;
 
+import no.fint.model.felles.kompleksedatatyper.Identifikator;
+
+import java.util.Map;
+
 public interface FintMainObject extends FintObject {
+
+    Map<String, Identifikator> getIdentifikators();
+
 }
