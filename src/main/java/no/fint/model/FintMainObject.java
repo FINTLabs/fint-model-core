8df0505f973ab1aa80ec155e8b45a653ac300ef8
Hknots <hknots99@gmail.com>
2024-02-16 11:31:21 +0100

    use FintIdentifikator

--- src/main/java/no/fint/model/FintMainObject.java
+++ src/main/java/no/fint/model/FintMainObject.java
@@ -1,11 +1,9 @@
 package no.fint.model;
 
-import no.fint.model.felles.kompleksedatatyper.Identifikator;
-
 import java.util.Map;
 
 public interface FintMainObject extends FintObject {
 
-    Map<String, Identifikator> getIdentifikators();
+    Map<String, FintIdentifikator> getIdentifikators();
 
 }
