090e044df424a24a89be5aa3b5d68a6f97a98961
Hknots <hknots99@gmail.com>
2024-02-18 14:38:25 +0100

    add javadoc & fix parameter of setIdentifikatorverdi

--- src/main/java/no/fint/model/FintIdentifikator.java
+++ src/main/java/no/fint/model/FintIdentifikator.java
@@ -1,8 +1,33 @@
 package no.fint.model;
 
+/**
+ * Represents an identifikator within the Fint model.
+ * <p>
+ * This interface is used for managing the identification value (identifikatorverdi) of an object.
+ * It provides methods to retrieve and set this value, facilitating the unique identification of
+ * objects within the Fint data model.
+ */
 public interface FintIdentifikator {
 
+    /**
+     * Retrieves the current value of the identifikator.
+     * <p>
+     * This method is used to obtain the identification value associated with an object, which
+     * is crucial for uniquely identifying it within the system.
+     *
+     * @return a string representing the current identifikator value.
+     */
     String getIdentifikatorverdi();
-    void setIdentifikatorverdi();
+
+    /**
+     * Sets the value of the identifikator.
+     * <p>
+     * This method allows for the identification value of an object to be updated. It is
+     * essential for maintaining the unique identification of objects, especially when changes
+     * to the identification criteria occur.
+     *
+     * @param identifikatorverdi the new value for the identifikator.
+     */
+    void setIdentifikatorverdi(String identifikatorverdi);
 
 }
