098d5135fcfdfff5dc45cefa3b24be22d76de9cd
Hknots <hknots99@gmail.com>
2024-04-24 12:35:50 +0200

    create new interfaces that extends FintMainObject

--- src/main/java/no/fint/model/FintMainObject.java
+++ src/main/java/no/fint/model/FintMainObject.java
@@ -27,18 +27,4 @@ public interface FintMainObject extends FintObject {
      */
     Map<String, FintIdentifikator> getIdentifikators();
 
-    /**
-     * Provides a list of relations to other {@code FintMainObject}s.
-     * <p>
-     * By default, this method returns an empty list, as not all main objects may have relations.
-     * Implementations can override this method to specify actual relations, facilitating
-     * the representation of relationships between different main objects within the model.
-     *
-     * @return a list of {@link FintRelation} objects representing the relationships this object
-     * has with other main objects. Returns an empty list by default.
-     */
-    default List<FintRelation> getRelations() {
-        return new ArrayList<>();
-    }
-
 }
---
+++ src/main/java/no/fint/model/FintResource.java
@@ -0,0 +1,4 @@
+package no.fint.model;
+
+public interface FintResource extends FintMainObject {
+}
