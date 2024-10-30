326c89b36c2d267e0bb56beef6df785377afd4ad
Henrik Knutsen <hknots99@gmail.com>
2024-10-30 10:16:06 +0100

    Add writeable (#6)
    * add writeable field
    
    * rename interface method

--- src/main/java/no/fint/model/FintModelObject.java
+++ src/main/java/no/fint/model/FintModelObject.java
@@ -1,4 +1,10 @@
 package no.fint.model;
 
 public interface FintModelObject extends FintMainObject {
+
+    /**
+     * Checks if the resource has any fields that are writable.
+     */
+    boolean isWriteable();
+
 }
